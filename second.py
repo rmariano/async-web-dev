9f331dfe75e379e0e4edb2168473efccf24f2f58
Mariano Anaya <marianoanaya@gmail.com>
2016-07-19 11:26:14 +0200

    Second example of web request with twisted
    It handles an HTTP view, that returns a delayed response. After 1s,
    returns the response based on the query pased as parameter.

---
+++ second.py
@@ -0,0 +1,32 @@
+from twisted.internet import reactor
+from twisted.web.server import Site, NOT_DONE_YET
+from twisted.web.resource import Resource
+
+
+class Greeting(Resource):
+    isLeaf = True
+
+    def delayed(self, request):
+        query = request.args.get(b'q', [""])[0]
+
+        request.write(b"Hello " + query + b"\n")
+        request.finish()
+
+    def render_GET(self, request):
+        reactor.callLater(1, self.delayed, request)
+        return NOT_DONE_YET
+
+
+resource = Greeting()
+factory = Site(resource)
+reactor.listenTCP(2001, factory)
+reactor.run()
+
+
+"""
+Calling like:
+    curl localhost:2001?q=Mariano
+
+Should reply:
+    Hello Mariano
+"""
